d9e2a2fb060204ab43f3039ed056ffe1cda7f104
AndrzejSwietek2001 <mr.andi321@gmail.com>
2022-12-01 01:58:06 +0100

    Added script

---
+++ Script/unzipper.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+FILE=""
+FOLDER=""
+INITIAL_PATH=$(pwd)
+REVERSE=0
+GR=0
+
+
+usage() {                                 # Function: Print a help message.
+  echo "Usage: $0 [ -r REVERSE ] [ -g GROUP ]" 1>&2
+}
+exit_abnormal() {                         # Function: Exit with error.
+  usage
+  exit 1
+}
+
+while getopts "r:g:p:" options;
+do
+  echo "$options"
+  case "${options}" in
+    r)
+      REVERSE=1
+      ;;
+    g)                                   # If the option is g,
+      GR=${OPTARG}
+      ;;
+    p)
+        FILE=${OPTARG}
+        FOLDER="${FILE%.zip}"
+      ;;
+    :)                                    # If expected argument omitted:
+      echo "Error: -${OPTARG} requires an argument."
+      exit_abnormal                       # Exit abnormally.
+      ;;
+    *)                                    # If unknown (any other) option:
+      exit_abnormal                       # Exit abnormally.
+      ;;
+  esac
+  # shift $(( OPTIND - 1 ));
+done
+
+
+handle_zip() {
+   CONTENT=$(ls $FOLDER/)
+    cd $FOLDER
+    echo $CONTENT
+
+    if [[ $( ls | wc -l) == 1 ]]
+    then
+        if [[ -d $CONTENT ]]
+        then
+            mv $CONTENT/* .
+            rm -rf $CONTENT
+        fi
+    fi
+
+    for x in *.zip; do
+        echo $x
+        SUB_ARCHIVE_FOLDER="${x%.zip}/*"
+        ls $SUB_ARCHIVE_FOLDER
+        unzip $x && mv $SUB_ARCHIVE_FOLDER $(pwd) && rm -rf $SUB_ARCHIVE_FOLDER
+    done
+
+    cd ..
+    if [[ $GR != 0 ]]
+        then
+            mkdir "$GR-$FOLDER"
+            mv "$FOLDER" "$GR-$FOLDER"
+            echo "[ARCHIVE]: $FILE -> $GR-$FOLDER"
+        else
+            echo "[ARCHIVE]: $FILE -> $FOLDER"
+    fi
+}
+
+# ZIP UNPACK
+if [[ "$FILE" == *.zip ]]
+then
+    unzip $FILE
+    handle_zip
+elif [[ "$FILE" == *.tar.gz ]]
+then
+    tar -xvf $FILE
+     handle_zip
+else
+    echo -e "INVALID FILE FORMAT \n \t Archive path: $FILE"
+fi
+
+
+
+cd $INITIAL_PATH
+exit 0
+
+# #!/bin/bash
+
+# # FILE=$1
+# # FOLDER="${FILE%.zip}"
+# FILE=""
+# FOLDER=""
+# INITIAL_PATH=$(pwd)
+# REVERSE=0
+# GR=0
+
+
+
+# usage() {                                 # Function: Print a help message.
+#   echo "Usage: $0 [ -r REVERSE ] [ -g GROUP ]" 1>&2
+# }
+# exit_abnormal() {                         # Function: Exit with error.
+#   usage
+#   exit 1
+# }
+
+# while getopts "rg:p:" options;
+# do        
+#   echo "$options"
+#   case "${options}" in                    
+#     r)                                    
+#       REVERSE=1                      
+#       ;;
+#     g)                                   # If the option is g,
+#       GR=${OPTARG}                   
+#       re_isanum='^[0-9]+$'                # Regex: match whole numbers only
+#       if ! [[ $GR =~ $re_isanum ]] ; then   
+#         echo "Error: TIMES must be a positive, whole number."
+#         exit_abnormal
+#       elif [ $GR -eq "0" ]; then       
+#         echo "Error: TIMES must be greater than zero."
+#         exit_abnormal                     # Exit abnormally.
+#       fi
+#       ;;
+#     p) 
+#         FILE=${OPTARG}
+#         FOLDER="${FILE%.zip}"
+#       ;;
+#     :)                                    # If expected argument omitted:
+#       echo "Error: -${OPTARG} requires an argument."
+#       exit_abnormal                       # Exit abnormally.
+#       ;;
+#     *)                                    # If unknown (any other) option:
+#       exit_abnormal                       # Exit abnormally.
+#       ;;
+#   esac
+#   shift
+# done
+
+
+# handle_zip() {
+#    CONTENT=$(ls $FOLDER/)
+#     cd $FOLDER
+#     echo $CONTENT
+
+#     if [[ $( ls | wc -l) == 1 ]] 
+#     then
+#         if [[ -d $CONTENT ]]
+#         then 
+#             mv $CONTENT/* .
+#             rm -rf $CONTENT
+#         fi
+#     fi
+
+#     for x in *.zip; do 
+#         echo $x
+#         SUB_ARCHIVE_FOLDER="${x%.zip}/*"
+#         ls $SUB_ARCHIVE_FOLDER
+#         unzip $x && mv $SUB_ARCHIVE_FOLDER $(pwd) && rm -rf $SUB_ARCHIVE_FOLDER
+#     done 
+# }
+
+# # ZIP UNPACK 
+# if [[ "$FILE" == *.zip ]] 
+# then
+#     unzip $FILE
+#     handle_zip
+# elif [[ "$FILE" == *.tar.gz ]] 
+# then
+#     tar -xvf $FILE
+#      handle_zip
+# else
+#     echo -e "INVALID FILE FORMAT \n \t Archive path: $FILE"
+# fi
+
+
+# echo $GR
+# if [[ $GR != 0 ]]
+# then
+#     mv $FOLDER "$GR-$FOLDER"
+#     echo "[ARCHIVE]: $FILE -> $GR-$FOLDER"
+# else 
+#     echo "[ARCHIVE]: $FILE -> $FOLDER"
+# fi
+
+# cd $INITIAL_PATH
+# exit 0
+
